a81f7cf2324c34bc6b84fe32cbfacd446afb0572
T-Max <tmax.tver.95@gmail.com>
2016-08-24 10:39:57 +0300

    Added "dobj_part_cache.h" header.

---
+++ src/dobj_part_cache.h
@@ -0,0 +1,25 @@
+#ifndef __DOBJ_PART_CACHE__
+#define __DOBJ_PART_CACHE__
+
+#include "q_shared.h"
+
+typedef struct DObjPartCacheVectorSet_s
+{
+  vec3_t forward;
+  vec3_t left;
+  vec3_t up;
+  vec3_t origin;
+} DObjPartCacheVectorSet_t;
+
+typedef struct DObjPartCache_s
+{
+	int svsFrameTime;
+	int entNum;
+	WORD partNameIdx;
+	WORD pad;
+	DObjPartCacheVectorSet_t vectorSet;
+} DObjPartCache_t;
+
+#define DOBJ_PART_CACHE (*(DObjPartCache_t*)0x08373250)
+
+#endif
