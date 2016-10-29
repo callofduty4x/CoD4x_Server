69b81fadf3155ff9e55ba0ce4545eb5fcb6c94a9
T-Max <tmax.tver.95@gmail.com>
2016-10-29 17:10:32 +0300

    q_math indentation

--- src/q_math.c
+++ src/q_math.c
@@ -4,185 +4,198 @@
 
 void Math_VectorToAngles(vec3_t vector, vec3_t angles)
 {
-	angles[0] = 0.0;
-	angles[1] = 0.0;
-	angles[2] = 0.0;
+    angles[0] = 0.0;
+    angles[1] = 0.0;
+    angles[2] = 0.0;
 
-	if ( vector[1] == 0.0 )
-	{
-		if ( vector[0] == 0.0 )
-		{
-			angles[0] = 90.0;
-			if ( vector[2] > 0.0 )
-				angles[0] = 270.0;
-		}
-	}
-	else
-	{
-		angles[0] = atan2(vector[2], sqrt(vector[0] * vector[0] + vector[1] * vector[1])) * -180.0 / M_PI;
-		angles[1] = atan2(vector[1], vector[0]) * 180.0 / M_PI;
+    if (vector[1] == 0.0)
+    {
+        if (vector[0] == 0.0)
+        {
+            angles[0] = 90.0;
+            if (vector[2] > 0.0)
+                angles[0] = 270.0;
+        }
+    }
+    else
+    {
+        angles[0] = atan2(vector[2], sqrt(vector[0] * vector[0] +
+                                          vector[1] * vector[1])) *
+                                          -180.0 / M_PI;
+        angles[1] = atan2(vector[1], vector[0]) * 180.0 / M_PI;
 
-		if ( angles[0] < 0.0 )
-			angles[0] += 360.0;
+        if (angles[0] < 0.0)
+            angles[0] += 360.0;
 
-		if ( angles[1] < 0.0 )
-			angles[1] += 360.0;
-	}
+        if (angles[1] < 0.0)
+            angles[1] += 360.0;
+    }
 }
 
 void Math_AnglesToVectors( const vec3_t angles, vec3_t forward, vec3_t right, vec3_t up )
 {
-	double angle;
-	static float sr, sp, sy, cr, cp, cy;
-	// static to help MS compiler fp bugs
+    double angle;
+    static float sr, sp, sy, cr, cp, cy;
+    // static to help MS compiler fp bugs
 
-	angle = angles[YAW] * (M_PI / 180.0);
-	sy = sin(angle);
-	cy = cos(angle);
+    angle = angles[YAW] * (M_PI / 180.0);
+    sy = sin(angle);
+    cy = cos(angle);
 
-	angle = angles[PITCH] * (M_PI / 180.0);
-	sp = sin(angle);
-	cp = cos(angle);
+    angle = angles[PITCH] * (M_PI / 180.0);
+    sp = sin(angle);
+    cp = cos(angle);
 
-	angle = angles[ROLL] * (M_PI / 180.0);
-	sr = sin(angle);
-	cr = cos(angle);
+    angle = angles[ROLL] * (M_PI / 180.0);
+    sr = sin(angle);
+    cr = cos(angle);
 
-	if(forward)
-	{
-		forward[0] = cp * cy;
-		forward[1] = cp * sy;
-		forward[2] = -sp;
-	}
-	if(right)
-	{
-		right[0] = ( -1 * sr * sp * cy + - 1 * cr * -sy );
-		right[1] = ( -1 * sr * sp * sy + - 1 * cr * cy );
-		right[2] = -1 * sr * cp;
-	}
-	if(up)
-	{
-		up[0] = ( cr * sp * cy + - sr * -sy );
-		up[1] = ( cr * sp * sy + - sr * cy );
-		up[2] = cr * cp;
-	}
+    if(forward)
+    {
+        forward[0] = cp * cy;
+        forward[1] = cp * sy;
+        forward[2] = -sp;
+    }
+    if(right)
+    {
+        right[0] = ( -1 * sr * sp * cy + - 1 * cr * -sy );
+        right[1] = ( -1 * sr * sp * sy + - 1 * cr * cy );
+        right[2] = -1 * sr * cp;
+    }
+    if(up)
+    {
+        up[0] = ( cr * sp * cy + - sr * -sy );
+        up[1] = ( cr * sp * sy + - sr * cy );
+        up[2] = cr * cp;
+    }
 }
 
 
 
 void AddLeanToPosition(float *position, const float fViewYaw, const float fLeanFrac, const float fViewRoll, const float fLeanDist)
 {
-  float v;
-  vec3_t v3ViewAngles;
-  vec3_t right;
+    float v;
+    vec3_t v3ViewAngles;
+    vec3_t right;
 
-  if ( fLeanFrac != 0.0 )
-  {
-    v = fLeanFrac * (2.0 - fabs(fLeanFrac));
-    v3ViewAngles[0] = 0.0;
-    v3ViewAngles[1] = fViewYaw;
-    v3ViewAngles[2] = fViewRoll * v;
-	Math_AnglesToVectors(v3ViewAngles, 0, right, 0);
-    VectorMA(position, v * fLeanDist, right, position);
-  }
+    if (fLeanFrac != 0.0)
+    {
+        v = fLeanFrac * (2.0 - fabs(fLeanFrac));
+        v3ViewAngles[0] = 0.0;
+        v3ViewAngles[1] = fViewYaw;
+        v3ViewAngles[2] = fViewRoll * v;
+        Math_AnglesToVectors(v3ViewAngles, 0, right, 0);
+        VectorMA(position, v * fLeanDist, right, position);
+    }
 }
 
 
 int BoxDistSqrdExceeds(const float *absmin, const float *absmax, const float *org, const float fogOpaqueDistSqrd)
 {
+    float f;
+    vec3_t ma;
+    vec3_t mi;
+    int i;
 
-  float f;
-  vec3_t ma;
-  vec3_t mi;
-  int i;
+    mi[0] = absmin[0] - org[0];
+    mi[1] = absmin[1] - org[1];
+    mi[2] = absmin[2] - org[2];
+    ma[0] = absmax[0] - org[0];
+    ma[1] = absmax[1] - org[1];
+    ma[2] = absmax[2] - org[2];
 
-  mi[0] = absmin[0] - org[0];
-  mi[1] = absmin[1] - org[1];
-  mi[2] = absmin[2] - org[2];
-  ma[0] = absmax[0] - org[0];
-  ma[1] = absmax[1] - org[1];
-  ma[2] = absmax[2] - org[2];
+    for(i = 0, f = 0.0; i < 3; ++i)
+    {
+        if ((float)(mi[i] * ma[i]) > 0.0)
+            f = f + fminf(ma[i] * ma[i], mi[i] * mi[i]);
+    }
 
-  for(i = 0, f = 0.0; i < 3; ++i)
-  {
-	if ( (float)(mi[i] * ma[i]) > 0.0 )
-	{
-      f = f + fminf(ma[i] * ma[i], mi[i] * mi[i]);
-	}
-	
-  }
-  
-  if(f > fogOpaqueDistSqrd)
-  {
-	  return qtrue;
-  }
-  return qfalse;
+    if(f > fogOpaqueDistSqrd)
+        return qtrue;
+
+    return qfalse;
 }
 
 int BoxOnPlaneSide( vec3_t emins, vec3_t emaxs, struct cplane_s *p ) {
-	float dist1, dist2;
-	int sides;
+    float dist1, dist2;
+    int sides;
 
-// fast axial cases
-	if ( p->type < 3 ) {
-		if ( p->dist <= emins[p->type] ) {
-			return 1;
-		}
-		if ( p->dist >= emaxs[p->type] ) {
-			return 2;
-		}
-		return 3;
-	}
+    // fast axial cases
+    if (p->type < 3)
+    {
+        if (p->dist <= emins[p->type])
+            return 1;
 
-// general case
-	switch ( p->signbits )
-	{
-	case 0:
-		dist1 = p->normal[0] * emaxs[0] + p->normal[1] * emaxs[1] + p->normal[2] * emaxs[2];
-		dist2 = p->normal[0] * emins[0] + p->normal[1] * emins[1] + p->normal[2] * emins[2];
-		break;
-	case 1:
-		dist1 = p->normal[0] * emins[0] + p->normal[1] * emaxs[1] + p->normal[2] * emaxs[2];
-		dist2 = p->normal[0] * emaxs[0] + p->normal[1] * emins[1] + p->normal[2] * emins[2];
-		break;
-	case 2:
-		dist1 = p->normal[0] * emaxs[0] + p->normal[1] * emins[1] + p->normal[2] * emaxs[2];
-		dist2 = p->normal[0] * emins[0] + p->normal[1] * emaxs[1] + p->normal[2] * emins[2];
-		break;
-	case 3:
-		dist1 = p->normal[0] * emins[0] + p->normal[1] * emins[1] + p->normal[2] * emaxs[2];
-		dist2 = p->normal[0] * emaxs[0] + p->normal[1] * emaxs[1] + p->normal[2] * emins[2];
-		break;
-	case 4:
-		dist1 = p->normal[0] * emaxs[0] + p->normal[1] * emaxs[1] + p->normal[2] * emins[2];
-		dist2 = p->normal[0] * emins[0] + p->normal[1] * emins[1] + p->normal[2] * emaxs[2];
-		break;
-	case 5:
-		dist1 = p->normal[0] * emins[0] + p->normal[1] * emaxs[1] + p->normal[2] * emins[2];
-		dist2 = p->normal[0] * emaxs[0] + p->normal[1] * emins[1] + p->normal[2] * emaxs[2];
-		break;
-	case 6:
-		dist1 = p->normal[0] * emaxs[0] + p->normal[1] * emins[1] + p->normal[2] * emins[2];
-		dist2 = p->normal[0] * emins[0] + p->normal[1] * emaxs[1] + p->normal[2] * emaxs[2];
-		break;
-	case 7:
-		dist1 = p->normal[0] * emins[0] + p->normal[1] * emins[1] + p->normal[2] * emins[2];
-		dist2 = p->normal[0] * emaxs[0] + p->normal[1] * emaxs[1] + p->normal[2] * emaxs[2];
-		break;
-	default:
-		dist1 = dist2 = 0;      // shut up compiler
-		break;
-	}
+        if (p->dist >= emaxs[p->type])
+            return 2;
 
-	sides = 0;
-	if ( dist1 >= p->dist ) {
-		sides = 1;
-	}
-	if ( dist2 < p->dist ) {
-		sides |= 2;
-	}
+        return 3;
+    }
 
-	return sides;
+    // general case
+    switch (p->signbits)
+    {
+        case 0:
+            dist1 = p->normal[0] * emaxs[0] + p->normal[1] * emaxs[1] +
+                    p->normal[2] * emaxs[2];
+            dist2 = p->normal[0] * emins[0] + p->normal[1] * emins[1] +
+                    p->normal[2] * emins[2];
+            break;
+        case 1:
+            dist1 = p->normal[0] * emins[0] + p->normal[1] * emaxs[1] +
+                    p->normal[2] * emaxs[2];
+            dist2 = p->normal[0] * emaxs[0] + p->normal[1] * emins[1] +
+                    p->normal[2] * emins[2];
+            break;
+        case 2:
+            dist1 = p->normal[0] * emaxs[0] + p->normal[1] * emins[1] +
+                    p->normal[2] * emaxs[2];
+            dist2 = p->normal[0] * emins[0] + p->normal[1] * emaxs[1] +
+                    p->normal[2] * emins[2];
+            break;
+        case 3:
+            dist1 = p->normal[0] * emins[0] + p->normal[1] * emins[1] +
+                    p->normal[2] * emaxs[2];
+            dist2 = p->normal[0] * emaxs[0] + p->normal[1] * emaxs[1] +
+                    p->normal[2] * emins[2];
+            break;
+        case 4:
+            dist1 = p->normal[0] * emaxs[0] + p->normal[1] * emaxs[1] +
+                    p->normal[2] * emins[2];
+            dist2 = p->normal[0] * emins[0] + p->normal[1] * emins[1] +
+                    p->normal[2] * emaxs[2];
+            break;
+        case 5:
+            dist1 = p->normal[0] * emins[0] + p->normal[1] * emaxs[1] +
+                    p->normal[2] * emins[2];
+            dist2 = p->normal[0] * emaxs[0] + p->normal[1] * emins[1] +
+                    p->normal[2] * emaxs[2];
+            break;
+        case 6:
+            dist1 = p->normal[0] * emaxs[0] + p->normal[1] * emins[1] +
+                    p->normal[2] * emins[2];
+            dist2 = p->normal[0] * emins[0] + p->normal[1] * emaxs[1] +
+                    p->normal[2] * emaxs[2];
+            break;
+        case 7:
+            dist1 = p->normal[0] * emins[0] + p->normal[1] * emins[1] +
+                    p->normal[2] * emins[2];
+            dist2 = p->normal[0] * emaxs[0] + p->normal[1] * emaxs[1] +
+                    p->normal[2] * emaxs[2];
+            break;
+        default:
+            dist1 = dist2 = 0; /* Shut up compiler. */
+            break;
+    }
+
+    sides = 0;
+    if (dist1 >= p->dist)
+        sides = 1;
+
+    if (dist2 < p->dist)
+        sides |= 2;
+
+    return sides;
 }
 
 /* to=from */
@@ -256,7 +269,7 @@ void vec2_rotate(vec2_t v, float pitch)
     vec2_t cp;
     vec2_copy(cp, v);
     float cosa = cosf(pitch*M_PI/180); /* In radians. */
-    float sina = sinf(pitch*M_PI/180);          /* In radians. */
+    float sina = sinf(pitch*M_PI/180); /* In radians. */
     v[0] = cp[0]*cosa - cp[1]*sina;
     v[1] = cp[0]*sina + cp[1]*cosa;
 }
--- src/q_math.h
+++ src/q_math.h
@@ -44,15 +44,15 @@ typedef int fixed4_t;
 typedef int fixed8_t;
 typedef int fixed16_t;
 
-#define DotProduct(a,b)			((a)[0]*(b)[0]+(a)[1]*(b)[1]+(a)[2]*(b)[2])
-#define VectorSubtract(a,b,c)	((c)[0]=(a)[0]-(b)[0],(c)[1]=(a)[1]-(b)[1],(c)[2]=(a)[2]-(b)[2])
-#define VectorAdd(a,b,c)		((c)[0]=(a)[0]+(b)[0],(c)[1]=(a)[1]+(b)[1],(c)[2]=(a)[2]+(b)[2])
-#define VectorCopy(a,b)			((b)[0]=(a)[0],(b)[1]=(a)[1],(b)[2]=(a)[2])
-//#define VectorCopy(a,b)			((b).x=(a).x,(b).y=(a).y,(b).z=(a).z])
+#define DotProduct(a,b)         ((a)[0]*(b)[0]+(a)[1]*(b)[1]+(a)[2]*(b)[2])
+#define VectorSubtract(a,b,c)   ((c)[0]=(a)[0]-(b)[0],(c)[1]=(a)[1]-(b)[1],(c)[2]=(a)[2]-(b)[2])
+#define VectorAdd(a,b,c)        ((c)[0]=(a)[0]+(b)[0],(c)[1]=(a)[1]+(b)[1],(c)[2]=(a)[2]+(b)[2])
+#define VectorCopy(a,b)         ((b)[0]=(a)[0],(b)[1]=(a)[1],(b)[2]=(a)[2])
+//#define VectorCopy(a,b)         ((b).x=(a).x,(b).y=(a).y,(b).z=(a).z])
 
-#define	VectorScale(v, s, o)	((o)[0]=(v)[0]*(s),(o)[1]=(v)[1]*(s),(o)[2]=(v)[2]*(s))
-#define VectorMA( v, s, b, o )  ( ( o )[0] = ( v )[0] + ( b )[0] * ( s ),( o )[1] = ( v )[1] + ( b )[1] * ( s ),( o )[2] = ( v )[2] + ( b )[2] * ( s ) )
-#define CrossProduct(a,b,c)		((c)[0]=(a)[1]*(b)[2]-(a)[2]*(b)[1],(c)[1]=(a)[2]*(b)[0]-(a)[0]*(b)[2],(c)[2]=(a)[0]*(b)[1]-(a)[1]*(b)[0])
+#define	VectorScale(v, s, o)    ((o)[0]=(v)[0]*(s),(o)[1]=(v)[1]*(s),(o)[2]=(v)[2]*(s))
+#define VectorMA(v, s, b, o)    ((o)[0]=(v)[0]+(b)[0]*(s),(o)[1]=(v)[1]+(b)[1]*(s),(o)[2]=(v)[2]+(b)[2]*(s))
+#define CrossProduct(a,b,c)     ((c)[0]=(a)[1]*(b)[2]-(a)[2]*(b)[1],(c)[1]=(a)[2]*(b)[0]-(a)[0]*(b)[2],(c)[2]=(a)[0]*(b)[1]-(a)[1]*(b)[0])
 
 #define DotProduct4( x,y )        ( ( x )[0] * ( y )[0] + ( x )[1] * ( y )[1] + ( x )[2] * ( y )[2] + ( x )[3] * ( y )[3] )
 #define VectorSubtract4( a,b,c )  ( ( c )[0] = ( a )[0] - ( b )[0],( c )[1] = ( a )[1] - ( b )[1],( c )[2] = ( a )[2] - ( b )[2],( c )[3] = ( a )[3] - ( b )[3] )
@@ -107,7 +107,7 @@ float vec3_maxabs    (vec3_t v);
 #define EQUAL_EPSILON   0.001
 #endif
 
-float Q_fabs( float f );
+float Q_fabs(float f);
 
 #ifndef ID_INLINE
 #ifdef _WIN32
