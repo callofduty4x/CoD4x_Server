79f894239db654f3cf8c88935ffcf37c4ce3600d
root <root@virt.test.com>
2017-04-08 08:36:27 -0400

    Fix for plugin udp send function

--- src/netchan.c
+++ src/netchan.c
@@ -477,8 +477,9 @@ qboolean NET_SendPacket( netsrc_t sock, int length, const void *data, netadr_t *
 	}
 
 	// sequenced packets are shown in netchan, so just show oob
-	if ( showpackets->boolean && *(int *)data == -1 )	{
-		Com_Printf ("send packet %4i\n", length);
+	if ( showpackets->boolean && *(int *)data == -1 ){
+		char string[128];
+		Com_Printf ("NET_SendPacket %4i destination %s\n", length, NET_AdrToStringMT(to, string, sizeof(string)));
 	}
 	if ( to->type == NA_LOOPBACK ) {
 		NET_SendLoopPacket (sock, length, data, *to);
