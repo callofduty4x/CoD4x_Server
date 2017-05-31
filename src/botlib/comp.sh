493a0b6e428ff99a85100c5dcbeb04d5c58ce467
AlexanderCurl <alexc@alexc.hu>
2017-05-31 22:44:08 +0100

    fix travis build

--- src/botlib/comp.sh
+++ src/botlib/comp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 gcc -m32 -D BOTLIB -c *.c
-# gcc -c ../q_math.c
-# gcc -c ../qshared.c
-# gcc -c ../qvsnprintf.c
+gcc -c ../q_math.c
+gcc -c ../qshared.c
+gcc -c ../qvsnprintf.c
 echo Linking
 ar rcs ../../lib/libbot.a *.o
 echo Cleaning up
