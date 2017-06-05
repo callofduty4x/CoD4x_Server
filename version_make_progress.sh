d786988e80f65a568434e3b3cfc7da631172b6f6
T-Max <tmax.tver.95@gmail.com>
2017-06-05 20:31:37 +0300

    build_version updater script updated %)

--- version_make_progress.sh
+++ version_make_progress.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # increments the real number held in version.h by 0.01
 
-read line < src/version_build.h
+read line < src/version/version_build.h
 tokens=($line)
 version=${tokens[2]}
 newversion=$(echo $version + 1 | bc)
-echo "#define BUILD_NUMBER $newversion" > src/version_build.h
+echo "#define BUILD_NUMBER $newversion" > src/version/version_build.h
