a37744a7bec94ddc6837e3f35ad98c3f31c56dd3
Kingwolf123 <usman_mad@yahoo.com>
2017-11-18 12:32:53 +0500

    [Antispam Plugin] Version 2.3

--- plugins/antispam/makeSO.sh
+++ plugins/antispam/makeSO.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo Compiling: release
-gcc -m32 -Wall -O1 -g -fvisibility=hidden -fpermissive -mtune=core2 -c *.cpp
+g++ -std=c++17 -m32 -Wall -s -O1 -mtune=core2 -c *.cpp
 
 echo Linking
-gcc -m32 -g -shared -o antispam.so *.o
+g++ -std=c++17 -m32 -Wall -s -shared -o antispam.so *.o
 echo Cleaning up
 rm *.o
-
