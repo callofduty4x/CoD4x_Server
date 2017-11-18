416636a1dfe1003ebba132d31681d6ccb327d4a5
Kingwolf123 <usman_mad@yahoo.com>
2017-11-18 12:32:53 +0500

    [Antispam Plugin] Version 2.3
    (cherry picked from commit a37744a7bec94ddc6837e3f35ad98c3f31c56dd3)

--- plugins/antispam/makeDLL.cmd
+++ plugins/antispam/makeDLL.cmd
@@ -1,13 +1,10 @@
 @echo off
 
-REM Compiling: debugging
-REM echo `gcc -g -m32 -Wall -O1 -s -mtune=core2 -c *.cpp`
-
 echo Compiling: release
-gcc -m32 -Wall -O1 -g -mtune=core2 -c *.cpp
+g++ -std=c++17 -m32 -Wall -O1 -s -mtune=core2 -c *.cpp
 
 echo Linking
-gcc -m32 -g -shared -static-libgcc -static-libstdc++ -o antispam.dll *.o -L..\ -lcom_plugin
+g++ -std=c++17 -m32 -s -shared -static-libgcc -static-libstdc++ -o antispam.dll *.o -L..\ -lcom_plugin
 echo Cleaning up
 del *.o
 
