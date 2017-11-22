93978f020285485b3cdc9f29d726ce20f1ebe0dd
rt <no@mail.com>
2017-11-22 16:00:40 +0100

    compile plugins with travis

--- plugins/plugins_deprecated/adminplugin/makedll.sh
+++ plugins/plugins_deprecated/adminplugin/makedll.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo Compiling: release
-gcc -m32 -Wall -O1 -g -fvisibility=hidden -mtune=core2 -c *.c
+gcc -m32 -Wall -O1 -g -fvisibility=hidden -mtune=core2 -c qshared.cpp main.cpp
 
 echo Linking
-gcc -m32 -g -shared -o pchat.so *.o
+gcc -m32 -g -shared -o sourcebansplugin.so *.o
 echo Cleaning up
 rm *.o
 
