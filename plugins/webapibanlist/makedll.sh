9b9aa036e75e0cfe6644fdb2a6ac03973fbb698c
Kingwolf123 <usman_mad@yahoo.com>
2017-11-30 21:17:29 +0500

    [Plugin Maintainence] added webapibanlist linux compile script

---
+++ plugins/webapibanlist/makedll.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo Compiling: release
+gcc -m32 -Wall -O1 -g -fvisibility=hidden -mtune=core2 -c *.c
+
+echo Linking
+gcc -m32 -g -shared -o webbanlist.so *.o
+echo Cleaning up
+rm *.o
+
+if [ $? -ne 0 ]
+then
+    echo "Compile failed!"
+    exit 1
+fi
