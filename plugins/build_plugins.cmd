b1126da8e98e0c14cda900b65f8791f9c9ed813d
D4edalus <D4edalus@users.noreply.github.com>
2017-12-15 17:38:27 +0100

    Update build_plugins.cmd

--- plugins/build_plugins.cmd
+++ plugins/build_plugins.cmd
@@ -1,36 +1,12 @@
 echo "Building Plugins -"
 
-cd screenshotsender
-call makedll_win32.cmd
-cd ..
-
+cd screenshotsender && call makedll_win32.cmd && cd ..
 cd sourcebansplugin && call makedll_win32.cmd && cd ..
-
-cd censor
-call makedll_win32.cmd
-cd ..
-
-cd cod4x_b3hide
-makedll_win32.cmd
-cd ..
-
-cd simplebanlist
-makedll_win32.cmd
-cd ..
-
-cd example_cpp_plugin
-makedll_win32.cmd
-cd ..
-
-cd pchat
-makedll_win32.cmd
-cd ..
-
-cd warn
-makedll_win32.cmd
-cd ..
-
-cd antispam
-makedll_win32.cmd
-cd ..
+cd censor && call makedll_win32.cmd && cd ..
+cd cod4x_b3hide && call makedll_win32.cmd && cd ..
+cd simplebanlist && call makedll_win32.cmd && cd ..
+cd example_cpp_plugin && call makedll_win32.cmd && cd ..
+cd pchat && call makedll_win32.cmd && cd ..
+cd warn && call makedll_win32.cmd && cd ..
+cd antispam && call makedll_win32.cmd && cd ..
 
