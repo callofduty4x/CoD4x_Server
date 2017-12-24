137092e98eeb1ab5aadbca7a6bad96bc01120c59
Kingwolf123 <usman_mad@yahoo.com>
2017-12-24 07:28:57 +0500

    [Antispam Plugin] Version 2.6

--- plugins/antispam/makedll_win32.cmd
+++ plugins/antispam/makedll_win32.cmd
@@ -1,14 +1,10 @@
 @echo off
 
 echo Compiling: release
-g++ -std=gnu++17 -m32 -Wall -s -O1 -mtune=core2 -c *.cpp || goto :error
+g++ -std=gnu++17 -m32 -Wall -O1 -mtune=core2 -c *.cpp
 
 echo Linking
-g++ -std=gnu++17 -m32 -Wall -s -shared -static-libgcc -static-libstdc++ -o antispam.dll *.o -L..\ -lcom_plugin || goto :error
+g++ -std=gnu++17 -m32 -Wall -shared -static-libgcc -static-libstdc++ -o antispam.dll *.o -L..\ -lcom_plugin
 echo Cleaning up
 del *.o
-
-goto :EOF
-:error
-echo Failed with error #%errorlevel%.
-exit /b 1
+pause
