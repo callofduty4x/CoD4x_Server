5730dccf3481f3d62143e784c768f379050df397
Beasty <beasty@cod4x.me>
2020-05-12 20:34:34 -0400

    Fixing reading HTTP chunked encoding

--- src/httpftp.c
+++ src/httpftp.c
@@ -689,7 +689,7 @@ static int HTTP_ProcessChunkedEncoding(ftRequest_t* request, qboolean connection
 			memmove(request->recvmsg.data + writeoffset, request->recvmsg.data + request->recvmsg.readcount, chunksize);
 			request->currentChunkLength += chunksize;
 			request->recvmsg.readcount += chunksize;
-			request->currentChunkReadOffset = request->recvmsg.readcount - request->headerLength;
+
 		}else if(connectionClosed){
 			return -1;
 		}else{
@@ -702,6 +702,7 @@ static int HTTP_ProcessChunkedEncoding(ftRequest_t* request, qboolean connection
 		{
 			return -1;
 		}
+		request->currentChunkReadOffset = request->recvmsg.readcount - request->headerLength;
 
 	}while(1);
 
